e1a1dc204d9b425204e415e691e22a4154497b3e
Ziyoga <ziyoda8523@gmail.com>
2019-01-29 21:51:28 -0500

    Add subtract method

--- src/Calculato.java
+++ src/Calculato.java
@@ -2,4 +2,8 @@ public class Calculato {
     public static double add(double num1,double num2){
         return num1 +num2;
     }
+    public static double subtract(double num1, double num2){
+        double result =num1 - num2;
+        return num1;
+    }
 }
